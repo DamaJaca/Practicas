5627a2e5cc404dae43d18b5512eed6bb99ffe063
DamaJaca <danielcardenete1994@gmail.com>
2024-04-16 13:16:06 +0200

    Añadido Dialog en RecyclerView de facturas

--- app/src/main/java/com/djcdev/practicas/ui/facturas/FacturasFragment.kt
+++ app/src/main/java/com/djcdev/practicas/ui/facturas/FacturasFragment.kt
@@ -1,16 +1,19 @@
 package com.djcdev.practicas.ui.facturas
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.djcdev.practicas.R
 import com.djcdev.practicas.databinding.FragmentFacturasBinding
@@ -18,7 +21,6 @@ import com.djcdev.practicas.domain.model.FacturaModel
 import com.djcdev.practicas.ui.facturas.adapter.FacturasAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-import androidx.appcompat.app.AppCompatActivity
 
 @AndroidEntryPoint
 class FacturasFragment : Fragment() {
@@ -86,7 +88,7 @@ class FacturasFragment : Fragment() {
     private fun initRecyclerView(facturas : List <FacturaModel>) {
 
         binding.progressBar.isVisible=false
-        facturasAdapter = FacturasAdapter()
+        facturasAdapter = FacturasAdapter(){onItemSelected()}
         facturasAdapter.updateList(facturas)
         binding.rvFacturas.apply {
             layoutManager = LinearLayoutManager(context)
@@ -95,4 +97,18 @@ class FacturasFragment : Fragment() {
 
 
     }
+
+    private fun onItemSelected() {
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.dialog_factura_information)
+        val btnClose : Button= dialog.findViewById(R.id.btnCloseDialogFacturas)
+        btnClose.setOnClickListener{
+            dialog.hide()
+        }
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.show()
+
+    }
+
+
 }
--- app/src/main/java/com/djcdev/practicas/ui/facturas/adapter/FacturasAdapter.kt
+++ app/src/main/java/com/djcdev/practicas/ui/facturas/adapter/FacturasAdapter.kt
@@ -1,5 +1,6 @@
 package com.djcdev.practicas.ui.facturas.adapter
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,7 +8,7 @@ import com.djcdev.practicas.R
 import com.djcdev.practicas.domain.model.FacturaModel
 import com.djcdev.practicas.domain.model.FacturasModel
 
-class FacturasAdapter (private var facturasList :List<FacturaModel> = emptyList()):RecyclerView.Adapter<FacturasViewHolder>(){
+class FacturasAdapter (private var facturasList :List<FacturaModel> = emptyList(), private val inSelected:()->Unit):RecyclerView.Adapter<FacturasViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FacturasViewHolder {
         return FacturasViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.item_factura, parent, false)
@@ -18,11 +19,13 @@ class FacturasAdapter (private var facturasList :List<FacturaModel> = emptyList(
 
     override fun onBindViewHolder(holder: FacturasViewHolder, position: Int) {
         holder.render (facturasList[position])
+        holder.itemView.setOnClickListener { inSelected () }
     }
 
     fun updateList(lista :List<FacturaModel>){
         facturasList = lista
         notifyDataSetChanged()
     }
+
 
 }
