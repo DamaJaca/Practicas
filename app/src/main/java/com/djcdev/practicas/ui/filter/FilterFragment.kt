acdd4511075029d3d1f57f446c8e1a2c49cd4b80
DamaJaca <danielcardenete1994@gmail.com>
2024-04-22 08:47:40 +0200

    Implementación del datePicker

--- app/src/main/java/com/djcdev/practicas/ui/filter/FilterFragment.kt
+++ app/src/main/java/com/djcdev/practicas/ui/filter/FilterFragment.kt
@@ -1,10 +1,12 @@
 package com.djcdev.practicas.ui.filter
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -19,6 +21,9 @@ import com.djcdev.practicas.ui.facturas.FacturasViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @AndroidEntryPoint
 class FilterFragment : Fragment() {
@@ -91,7 +96,31 @@ class FilterFragment : Fragment() {
             )
             requireActivity().onBackPressedDispatcher.onBackPressed()
         }
+        binding.btnFromFacturasFilter.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val datePickerDialog = DatePickerDialog(
+                requireContext(), {DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                    // Create a new Calendar instance to hold the selected date
+                    val selectedDate = Calendar.getInstance()
+                    // Set the selected date using the values received from the DatePicker dialog
+                    selectedDate.set(year, monthOfYear, dayOfMonth)
+                    // Create a SimpleDateFormat to format the date as "dd/MM/yyyy"
+                    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                    // Format the selected date into a string
+                    val formattedDate = dateFormat.format(selectedDate.time)
+                    // Update the TextView to display the selected date with the "Selected Date: " prefix
+                    binding.btnFromFacturasFilter.text = formattedDate
+                },
+
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            )
+            // Show the DatePicker dialog
+            datePickerDialog.show()
+        }
 
 
     }
+
 }
