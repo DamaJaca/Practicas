12c3eeac23474880a95fc7c526d80fd241edded9
DamaJaca <danielcardenete1994@gmail.com>
2024-04-23 12:29:24 +0200

    Limpieza de nonNulls

--- app/src/main/java/com/djcdev/practicas/ui/smartsolar/pagesfromtabs/DetailsFragment.kt
+++ app/src/main/java/com/djcdev/practicas/ui/smartsolar/pagesfromtabs/DetailsFragment.kt
@@ -2,31 +2,17 @@ package com.djcdev.practicas.ui.smartsolar.pagesfromtabs
 
 import android.app.Dialog
 import android.os.Bundle
-import android.text.Editable
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
-import com.djcdev.practicas.R
 import com.djcdev.practicas.databinding.DialogDetailsBinding
 import com.djcdev.practicas.databinding.FragmentDetailsBinding
-import com.djcdev.practicas.databinding.FragmentEnergyBinding
-import com.djcdev.practicas.domain.model.DetailModel
-import com.djcdev.practicas.domain.usecase.GetDetailsUseCase
-import com.djcdev.practicas.ui.facturas.FacturasState
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment() {
--- app/src/main/java/com/djcdev/practicas/ui/smartsolar/pagesfromtabs/EnergyFragment.kt
+++ app/src/main/java/com/djcdev/practicas/ui/smartsolar/pagesfromtabs/EnergyFragment.kt
@@ -1,13 +1,11 @@
 package com.djcdev.practicas.ui.smartsolar.pagesfromtabs
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.djcdev.practicas.R
+import androidx.fragment.app.Fragment
 import com.djcdev.practicas.databinding.FragmentEnergyBinding
-import com.djcdev.practicas.databinding.FragmentMiInstalacionBinding
 
 
 class EnergyFragment : Fragment() {
@@ -18,7 +16,7 @@ class EnergyFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binging = FragmentEnergyBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
